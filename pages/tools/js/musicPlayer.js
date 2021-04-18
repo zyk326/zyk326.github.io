261b98006085f9aed1618cc874fdf8d641f1affe
zyk326 <869550165@qq.com>
2021-04-18 23:51:03 +0800

    musicView

--- pages/tools/js/musicPlayer.js
+++ pages/tools/js/musicPlayer.js
@@ -9,7 +9,40 @@ let musicPlayer = new Vue({
         // 搜索关键字
         keyword: '',
         // 有关关键字的信息
-        getinfoli: '',
+        getinfoli: [{
+            "id": 108428,
+            "name": "爱不会绝迹",
+            "artists": [
+                {
+                    "name": "林俊杰",
+                }
+            ],
+            "mvid": 5501557,
+
+        }],
+        songinfo: [
+            {
+                "data": {
+                    "data": [
+                        {
+                            "url": "http://m8.music.126.net/20210418195349/a59ed4f79e7b86be61e702081f03d20b/ymusic/ts/free/bf52b82d46ab06766919fde4f7aa8666.mp3",
+                        }
+                    ],
+                },
+            }
+        ],
+        pocinfo: [{
+            "data": {
+                "songs": [
+                    {
+                        "al": {
+                            "picUrl": "https://p2.music.126.net/rCKv3f3MgBkZ1aY1eHYn8A==/109951165628175250.jpg",
+                        },
+                    }
+                ]
+            }
+        }],
+        cominfo: [],
         // 音乐链接
         musicurl: '',
         // 演唱者
@@ -43,23 +76,26 @@ let musicPlayer = new Vue({
             let that = this;
             axios.get("https://autumnfish.cn/song/url?id=" + this.getinfoli[e].id)
                 .then(function (response) {
-                    console.log(response)
-                    that.musicurl = response.data.data[0].url;
-                    // that.picture = response
+                    // console.log(response)
+                    that.songinfo = response;
+                    that.musicurl = that.songinfo.data.data[0].url;
                 }, function (err) {
 
                 })
             axios.get("https://autumnfish.cn/song/detail?ids=" + this.getinfoli[e].id)
                 .then(function (response) {
-                    console.log(response)
-                    that.picture = response.data.songs[0].al.picUrl;
+                    // console.log(response)
+                    that.pocinfo = response;
+                    that.picture = that.pocinfo.data.songs[0].al.picUrl;
+                    console.log('45454545' + that.pocinfo.data.songs[0].al.picUrl)
                 }, function (err) {
 
                 })
             axios.get("https://autumnfish.cn/comment/hot?type=0&id=" + this.getinfoli[e].id)
                 .then(function (response) {
-                    console.log(response)
-                    that.comInfo = response.data.hotComments;
+                    // console.log(response)
+                    that.cominfo = response
+                    that.comInfo = that.cominfo.data.hotComments;
                 }, function (err) {
 
                 })
@@ -81,7 +117,7 @@ let musicPlayer = new Vue({
             let that = this;
             axios.get("https://autumnfish.cn/mv/url?id=" + e)
                 .then(function (response) {
-                    // console.log(response.data.data.url)
+                    console.log(response.data.data.url)
                     that.mvUrl = response.data.data.url;
                     that.flagMv = true;
                 }, function (err) {
